d45e63baa86e06587f60a8eb6aae6fd9cddbba85
gbs <gabriel.palhano@escolar.ifrn.edu.br>
2024-09-03 14:22:58 -0300

    meu joguinho

---
+++ Meu Game 3D/Assets/scripts/moeda.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        transform.Rotate(eulers:Vector3.forward * velocidadeGiro * Time.deltaTime);
+    }
+}
+
+
+
+
